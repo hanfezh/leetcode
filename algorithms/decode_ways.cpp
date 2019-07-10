699a9633627b4200edaa5d86874d77972d62ff76
ufengzh <xianfeng.zhu@gmail.com>
2019-07-10 11:55:08 +0800

    Using non-recursive solution to solve problem 91 `Decode Ways`.

--- algorithms/decode_ways.cpp
+++ algorithms/decode_ways.cpp
@@ -17,8 +17,14 @@
  */
 #include <stdio.h>
 #include <string>
+#include <vector>
 
-class Solution
+class Solution1;
+class Solution2;
+using Solution = Solution2;
+
+// Recursive
+class Solution1
 {
 public:
     int numDecodings(std::string& str)
@@ -55,9 +61,45 @@ private:
     }
 };
 
+// Non-recursive
+class Solution2
+{
+public:
+    int numDecodings(std::string& str)
+    {
+        if (str.size() == 0)
+        {
+            return 0;
+        }
+
+        std::vector<int> dp(str.size() + 1, 0);
+        dp[str.size()] = 1;
+        dp[str.size() - 1] = ((str[str.size() - 1] != '0') ? 1 : 0);
+        for (int i = str.size() - 2; i >= 0; i--)
+        {
+            if (str[i] == '0')
+            {
+                continue;
+            }
+            int val = (str[i] - '0') * 10 + (str[i + 1] - '0');
+            if (val >= 1 && val <= 26)
+            {
+                dp[i] = dp[i + 1] + dp[i + 2];
+            }
+            else
+            {
+                dp[i] = dp[i + 1];
+            }
+        }
+
+        return dp[0];
+    }
+};
+
 int main(int argc, char* argv[])
 {
-    std::string str = "12";
+    // std::string str = "12";
+    std::string str = "6065812287883668764831544958683283296479682877898293612168136334983851946827579555449329483852397155";
     int num = Solution().numDecodings(str);
     printf("Input: %s\nWays: %d\n", str.c_str(), num);
     return 0;
