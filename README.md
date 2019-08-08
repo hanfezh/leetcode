6b093d960cfed9b764f766d8d82b1ec04a106a0e
ufengzh <xianfeng.zhu@gmail.com>
2019-08-08 10:52:03 +0800

    Solve problem 404 `Sum of Left Leaves`.

--- README.md
+++ README.md
@@ -130,6 +130,7 @@
 | 377 | [Combination Sum IV](https://leetcode.com/problems/combination-sum-iv) | Medium | [C++](./algorithms/combination_sum4.cpp) |
 | 386 | [Lexicographical Numbers](https://leetcode.com/problems/lexicographical-numbers) | Medium | [C++](./algorithms/lexicographical_numbers.cpp) |
 | 397 | [Integer Replacement](https://leetcode.com/problems/integer-replacement) | Medium | [C++](./algorithms/integer_replacement.cpp) |
+| 404 | [Sum of Left Leaves](https://leetcode.com/problems/sum-of-left-leaves) | Easy | [C++](./algorithms/sum_of_left_leaves.cpp) | _Tree_ |
 | 415 | [Add Strings](https://leetcode.com/problems/add-strings) | Easy | [C++](./algorithms/add_strings.cpp) |
 | 437 | [Path Sum III](https://leetcode.com/problems/path-sum-iii) | Easy | [C++](./algorithms/path_sum3.cpp) |
 | 442 | [Find All Duplicates in an Array](https://leetcode.com/problems/find-all-duplicates-in-an-array/) | Medium | [C++](./algorithms/find_duplicates.cpp) |
---
+++ algorithms/sum_of_left_leaves.cpp
@@ -0,0 +1,61 @@
+// =====================================================================================
+//
+//       Filename:  sum_of_left_leaves.cpp
+//
+//    Description:  404. Sum of Left Leaves. 
+//                  Find the sum of all left leaves in a given binary tree.
+//
+//        Version:  1.0
+//        Created:  08/08/2019 10:35:59 AM
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Zhu Xianfeng (), xianfengzhu@tencent.com
+//   Organization:  
+//
+// =====================================================================================
+#include <stdio.h>
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution
+{
+public:
+    int sumOfLeftLeaves(TreeNode* root)
+    {
+        int sum = 0;
+        sumOfLeftLeaves(root, false, &sum);
+        return sum;
+    }
+
+private:
+    void sumOfLeftLeaves(TreeNode* node, bool is_left, int* sum)
+    {
+        if (node == nullptr)
+        {
+            return;
+        }
+        if (node->left == nullptr && node->right == nullptr && is_left)
+        {
+            // Current node is leaf
+            *sum += node->val;
+        }
+
+        sumOfLeftLeaves(node->left, true, sum);
+        sumOfLeftLeaves(node->right, false, sum);
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    TreeNode* root = nullptr;
+    auto sum = Solution().sumOfLeftLeaves(root);
+    printf("Sum of left leaves? %d\n", sum);
+    return 0;
+}
