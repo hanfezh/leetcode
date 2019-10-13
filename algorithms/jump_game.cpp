80ac37cb9aaf19d980557b4dd40d8ab6873dba1d
ufengzh <xianfeng.zhu@gmail.com>
2019-10-13 11:43:40 +0800

    Using dynamic programming to solve problem 55 `Jump Game`.

---
+++ algorithms/jump_game.cpp
@@ -0,0 +1,82 @@
+// 55. Jump Game: https://leetcode.com/problems/jump-game
+// Author: xianfeng.zhu@gmail.com
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+
+// Backtracking, time limit exceeded
+class Solution1
+{
+public:
+    bool canJump(std::vector<int>& nums)
+    {
+        return canJump(nums, 0);
+    }
+
+private:
+    bool canJump(std::vector<int>& nums, int start)
+    {
+        if (start >= nums.size() - 1)
+        {
+            return true;
+        }
+
+        int end = start + nums[start];
+        for (int i = end; i > start; i--)
+        {
+            if (canJump(nums, i))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+// Dynamic programming
+class Solution2
+{
+public:
+    enum JumpState
+    {
+        kBad,
+        kGood,
+    };
+
+    bool canJump(std::vector<int>& nums)
+    {
+        if (nums.size() == 0)
+        {
+            return true;
+        }
+
+        std::vector<JumpState> states(nums.size(), kBad);
+        states[nums.size() - 1] = kGood;
+
+        for (int i = nums.size() - 2; i >= 0; i--)
+        {
+            int end = std::min(nums[i] + i, (int)(nums.size() - 1));
+            for (int j = i + 1; j <= end; j++)
+            {
+                if (states[j] == kGood)
+                {
+                    states[i] = kGood;
+                    break;
+                }
+            }
+        }
+
+        return (states[0] == kGood);
+    }
+};
+
+using Solution = Solution2;
+
+int main(int argc, char* argv[])
+{
+    std::vector<int> nums = {3, 2, 1, 0, 4};
+    bool is_jump = Solution().canJump(nums);
+    printf("Can jump? %s\n", (is_jump ? "Yes" : "No"));
+    return 0;
+}
