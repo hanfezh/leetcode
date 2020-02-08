a914b878f346f4ada1c22cc66c480c7b1cc7fdaa
ufengzh <xianfeng.zhu@gmail.com>
2020-02-08 11:57:53 +0800

    Use kmp to implement strstr.

--- README.md
+++ README.md
@@ -29,7 +29,7 @@
 | 25 | [Reverse Nodes in k-Group](https://leetcode.com/problems/reverse-nodes-in-k-group) | Hard | [C++](./algorithms/reverse_nodes_in_k_group.cpp) | |
 | 26 | [Remove Duplicates from Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array) | Easy | [Python](./algorithms/remove_duplicates.py) |
 | 27 | [Remove Element](https://leetcode.com/problems/remove-element) | Easy | [Python](./algorithms/remove_element.py) |
-| 28 | [Implement strStr()](https://leetcode.com/problems/implement-strstr) | Easy | [C++](./algorithms/str_str.cpp) |
+| 28 | [Implement strStr()](https://leetcode.com/problems/implement-strstr) | Easy | [C++](./algorithms/str_str.cpp) | `String` `KMP` |
 | 29 | [Divide Two Integers](https://leetcode.com/problems/divide-two-integers) | Medium | [C++](./algorithms/divide_two_integers.cpp) |
 | 30 | [Substring with Concatenation of All Words](https://leetcode.com/problems/substring-with-concatenation-of-all-words) | Hard | [C++](./algorithms/substring_with_concatenation_words.cpp) | `Hash Table` |
 | 31 | [Next Permutation](https://leetcode.com/problems/next-permutation) | Medium | [C++](./algorithms/next_permutation.cpp) |
--- algorithms/str_str.cpp
+++ algorithms/str_str.cpp
@@ -3,7 +3,7 @@
  *
  *       Filename:  str_str.cpp
  *
- *    Description:  Implement strStr(). 
+ *    Description:  28. Implement strStr().
  *                  Return the index of the first occurrence of needle in haystack, or 
  *                  -1 if needle is not part of haystack.
  *
@@ -21,38 +21,114 @@
 #include <string>
 #include <vector>
 
-class Solution
+// Brute-force
+class Solution1
 {
 public:
     int strStr(const std::string& haystack, const std::string& needle)
     {
-        if (needle.empty())
+        int n = haystack.size();
+        int m = needle.size();
+
+        for (int i = 0; i <= n - m; i++)
+        {
+            int j = 0;
+            for ( ; j < m; j++)
+            {
+                if (haystack[i + j] != needle[j])
+                {
+                    break;
+                }
+            }
+            if (j == m)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
+
+// KMP
+class Solution2
+{
+public:
+    int strStr(const std::string& haystack, const std::string& needle)
+    {
+        int n = haystack.size();
+        int m = needle.size();
+        if (m == 0)
         {
             return 0;
         }
 
-        int idx = -1;
-        for (size_t i = 0; i < haystack.size(); i++)
+        auto lps = computeLps(needle);
+        int i = 0;
+        int j = 0;
+
+        while (i < n)
         {
-            if ((i + needle.size()) > haystack.size())
+            if (j < m && haystack[i] == needle[j])
             {
-                break;
+                i++;
+                j++;
             }
-            if (haystack.substr(i, needle.size()) == needle)
+            if (j == m)
             {
-                idx = i;
-                break;
+                // Found needle
+                return i - j;
+            }
+            else if (i < n && haystack[i] != needle[j])
+            {
+                if (j != 0)
+                {
+                    j = lps[j - 1];
+                }
+                else
+                {
+                    i++;
+                }
             }
         }
 
-        return idx;
+        return -1;
+    }
+
+private:
+    std::vector<int> computeLps(const std::string& needle)
+    {
+        std::vector<int> lps(needle.size(), 0);
+        int len = 0;
+        int idx = 1;
+
+        while (idx < needle.size())
+        {
+            if (needle[idx] == needle[len])
+            {
+                len++;
+                lps[idx++] = len;
+            }
+            else if (len != 0)
+            {
+                len = lps[len - 1];
+            }
+            else
+            {
+                lps[idx++] = 0;
+            }
+        }
+
+        return lps;
     }
 };
 
+using Solution = Solution2;
+
 int main(int argc, char* argv[])
 {
-    std::string haystack = "hello";
-    std::string needle = "ll";
+    std::string haystack = "mississippi";
+    std::string needle = "issip";
     if (argc > 2)
     {
         haystack = argv[1];
