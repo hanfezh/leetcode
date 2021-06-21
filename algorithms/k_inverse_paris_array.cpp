6dd6541e9a1c6e867c79f9ca0100307a9b49da98
hanfezh <xianfeng.zhu@gmail.com>
2021-06-21 19:53:05 +0800

    Solve problem 629, still time limit exceeded.

--- algorithms/k_inverse_paris_array.cpp
+++ algorithms/k_inverse_paris_array.cpp
@@ -6,8 +6,8 @@
 #include <vector>
 #include <map>
 
-// Time Limit Exceeded
-class Solution {
+// Brutal force, time limit exceeded
+class Solution1 {
  public:
   int kInversePairs(int n, int k) {
     if (n < 1) {
@@ -49,6 +49,61 @@ class Solution {
     return pairs[k];
   }
 };
+
+// Dynamic programming
+class Solution2 {
+ public:
+  int kInversePairs(int n, int k) {
+    if (n < 1 || k < 0) {
+      return 0;
+    }
+    // Implementation logic:
+    // 1. pair size, s(n)
+    //    s(n) = s(n-1) + n - 1
+    // 2. number for pairs, f(n)(k)
+    //    f(n)(k) = f(n)(k-1) + f(n-n)(k)
+    //    if (k > n) {
+    //      f(n)(k) -= f(n-1)(k-n);
+    //    }
+
+    // Caculate pairs size
+    std::vector<int> sizes(n + 1);
+    sizes[0] = 1;
+    for (int i = 1; i <= n; i++) {
+      sizes[i] = sizes[i - 1] + i - 1;
+    }
+
+    // Verify k
+    if (k >= sizes[n]) {
+      // Invalid k
+      return 0;
+    } else if (k >= sizes[n] / 2) {
+      // Correct k to first half
+      k = sizes[n] - k - 1;
+    }
+
+    // Allocate arrays to store numbers
+    std::vector<std::vector<int>> nums(n + 1, std::vector<int>(k + 1));
+
+    // Caculate numbers
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j <= k; j++) {
+        if (j == 0) {
+          nums[i][j] = 1;
+        } else if (j < sizes[i]) {
+          nums[i][j] = nums[i][j - 1] + nums[i - 1][j];
+          if (j >= i) {
+            nums[i][j] -= nums[i - 1][j - i];
+          }
+        }
+      }
+    }
+
+    return nums[n][k];
+  }
+};
+
+using Solution = Solution2;
 
 int main(int argc, char* argv[]) {
   int n = 3;
