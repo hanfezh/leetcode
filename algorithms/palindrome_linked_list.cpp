8f7ebeeaf58956ba3b4b30bbe976417f3725002e
hanfezh <xianfeng.zhu@gmail.com>
2021-09-19 12:34:52 +0800

    Check palindrome linked list by two pointers.

--- algorithms/palindrome_linked_list.cpp
+++ algorithms/palindrome_linked_list.cpp
@@ -3,45 +3,72 @@
 
 #include <stdio.h>
 
-struct ListNode
-{
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(nullptr) { }
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode(int x) : val(x), next(nullptr) {}
 };
 
 // Recursive
-class Solution
-{
-public:
-    bool isPalindrome(ListNode* head)
-    {
-        return isPalindrome(&head, head);
+class Solution1 {
+ public:
+  bool isPalindrome(ListNode* head) { return isPalindrome(&head, head); }
+
+ private:
+  bool isPalindrome(ListNode** head, ListNode* tail) {
+    if (tail == nullptr) {
+      return true;
     }
 
-private:
-    bool isPalindrome(ListNode** head, ListNode* tail)
-    {
-        if (tail == nullptr)
-        {
-            return true;
-        }
-
-        bool is_pal = isPalindrome(head, tail->next);
-        if (!is_pal || (*head)->val != tail->val)
-        {
-            return false;
-        }
-
-        *head = (*head)->next;
-        return true;
+    bool is_pal = isPalindrome(head, tail->next);
+    if (!is_pal || (*head)->val != tail->val) {
+      return false;
     }
+
+    *head = (*head)->next;
+    return true;
+  }
 };
 
-int main(int argc, char* argv[])
-{
-    ListNode* head = nullptr;
-    bool is_pal = Solution().isPalindrome(head);
-    printf("Is palindrome? %s\n", (is_pal ? "Yes" : "No"));
-    return 0;
+// Two pointers, reverse first half of linked list
+class Solution2 {
+ public:
+  bool isPalindrome(ListNode* head) {
+    ListNode dummy(0);
+    ListNode* ptr = &dummy;
+    ListNode* tmp = nullptr;
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (slow != nullptr && fast != nullptr) {
+      if (fast->next == nullptr) {
+        slow = slow->next;
+        break;
+      }
+      fast = fast->next->next;
+      tmp = slow->next;
+      slow->next = ptr->next;
+      ptr->next = slow;
+      slow = tmp;
+    }
+
+    ptr = dummy.next;
+    while (slow != nullptr) {
+      if (ptr->val != slow->val) {
+        return false;
+      }
+      slow = slow->next;
+      ptr = ptr->next;
+    }
+    return true;
+  }
+};
+
+using Solution = Solution2;
+
+int main(int argc, char* argv[]) {
+  ListNode* head = nullptr;
+  bool is_pal = Solution().isPalindrome(head);
+  printf("Is palindrome? %s\n", (is_pal ? "Yes" : "No"));
+  return 0;
 }
