5fc074c92334678d893e21584bcb055f79eed68a
hanfezh <xianfeng.zhu@gmail.com>
2021-10-12 23:13:42 +0800

    Use binary search to find single element in a sorted array.

--- algorithms/single_non_duplicate.cpp
+++ algorithms/single_non_duplicate.cpp
@@ -18,7 +18,8 @@
  */
 #include <vector>
 
-class Solution {
+// Bit manipulation, O(n)
+class Solution1 {
  public:
   int singleNonDuplicate(std::vector<int>& nums) {
     int once = 0;
@@ -28,6 +29,34 @@ class Solution {
     return once;
   }
 };
+
+// Binary search, O(logn)
+class Solution2 {
+ public:
+  int singleNonDuplicate(std::vector<int>& nums) {
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left < right) {
+      const int mid = (left + right) / 2;
+      if ((mid % 2) == 0) {
+        if (nums[mid] == nums[mid + 1]) {
+          left = mid + 2;
+        } else {
+          right = mid;
+        }
+      } else {
+        if (nums[mid] == nums[mid + 1]) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
+        }
+      }
+    }
+    return nums[left];
+  }
+};
+
+using Solution = Solution2;
 
 int main(int argc, char* argv[]) {
   std::vector<int> nums = {1, 1, 2, 3, 3, 4, 4, 8, 8};
