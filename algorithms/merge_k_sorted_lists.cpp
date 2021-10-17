7f8f96d62686830df0840473b82e7f33375ce7b5
hanfezh <xianfeng.zhu@gmail.com>
2021-10-17 09:43:59 +0800

    A little bit of optimization to merge k sorted lists.

--- algorithms/merge_k_sorted_lists.cpp
+++ algorithms/merge_k_sorted_lists.cpp
@@ -3,7 +3,7 @@
  *
  *       Filename:  merge_k_sorted_lists.cpp
  *
- *    Description:  23. Merge k Sorted Lists. Merge k sorted linked lists and return it 
+ *    Description:  23. Merge k Sorted Lists. Merge k sorted linked lists and return it
  *                  as one sorted list. Analyze and describe its complexity.
  *
  *        Version:  1.0
@@ -12,7 +12,7 @@
  *       Compiler:  gcc
  *
  *         Author:  Zhu Xianfeng (), xianfeng.zhu@gmail.com
- *   Organization:  
+ *   Organization:
  *
  * =====================================================================================
  */
@@ -20,71 +20,53 @@
 #include <stdlib.h>
 #include <vector>
 
-struct ListNode
-{
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode(int x) : val(x), next(nullptr) {}
 };
 
-class Solution
-{
-public:
-    ListNode* mergeKLists(const std::vector<ListNode*>& lists)
-    {
-        if (lists.empty())
-        {
-            return NULL;
-        }
-
-        ListNode* first = lists[0];
-        for (int i = 1; i < lists.size(); i++)
-        {
-            first = mergeList(first, lists[i]);
-        }
-
-        return first;
+class Solution {
+ public:
+  ListNode* mergeKLists(const std::vector<ListNode*>& lists) {
+    ListNode* first = nullptr;
+    for (int i = 0; i < lists.size(); i++) {
+      first = mergeList(first, lists[i]);
     }
 
-private:
-    ListNode* mergeList(ListNode* a, ListNode* b)
-    {
-        ListNode fake(0);
-        ListNode* ptr = &fake;
-        while (a != NULL && b != NULL)
-        {
-            if (a->val < b->val)
-            {
-                ptr->next = a;
-                a = a->next;
-                ptr = ptr->next;
-                ptr->next = NULL;
-            }
-            else
-            {
-                ptr->next = b;
-                b = b->next;
-                ptr = ptr->next;
-                ptr->next = NULL;
-            }
-        }
+    return first;
+  }
 
-        if (a != NULL)
-        {
-            ptr->next = a;
-        }
-        else if (b != NULL)
-        {
-            ptr->next = b;
-        }
-
-        return fake.next;
+ private:
+  ListNode* mergeList(ListNode* a, ListNode* b) {
+    ListNode fake(0);
+    ListNode* ptr = &fake;
+    while (a != nullptr && b != nullptr) {
+      ListNode* tmp = nullptr;
+      if (a->val < b->val) {
+        tmp = a;
+        a = a->next;
+      } else {
+        tmp = b;
+        b = b->next;
+      }
+      ptr->next = tmp;
+      ptr = ptr->next;
+      ptr->next = nullptr;
     }
+
+    if (a != nullptr) {
+      ptr->next = a;
+    } else if (b != nullptr) {
+      ptr->next = b;
+    }
+
+    return fake.next;
+  }
 };
 
-int main(int argc, char* argv[])
-{
-    std::vector<ListNode*> lists;
-    auto* sorted = Solution().mergeKLists(lists);
-    return 0;
+int main(int argc, char* argv[]) {
+  std::vector<ListNode*> lists;
+  auto* sorted = Solution().mergeKLists(lists);
+  return 0;
 }
