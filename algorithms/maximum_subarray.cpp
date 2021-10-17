f459a29774d172ded394e26d071bb03023266a5e
hanfezh <xianfeng.zhu@gmail.com>
2021-10-17 22:31:22 +0800

    A little bit of optimization to find max subarray.

--- algorithms/maximum_subarray.cpp
+++ algorithms/maximum_subarray.cpp
@@ -3,8 +3,8 @@
  *
  *       Filename:  maximum_subarray.cpp
  *
- *    Description:  Maximum Subarray: Given an integer array nums, find the contiguous 
- *                  subarray (containing at least one number) which has the largest sum 
+ *    Description:  Maximum Subarray: Given an integer array nums, find the contiguous
+ *                  subarray (containing at least one number) which has the largest sum
  *                  and return its sum.
  *
  *        Version:  1.0
@@ -13,58 +13,45 @@
  *       Compiler:  gcc
  *
  *         Author:  Zhu Xianfeng (), xianfeng.zhu@gmail.com
- *   Organization:  
+ *   Organization:
  *
  * =====================================================================================
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
 #include <vector>
 
-class Solution
-{
-public:
-    int maxSubArray(const std::vector<int>& nums)
-    {
-        if (nums.size() == 0)
-        {
-            return 0;
-        }
-
-        int max_sum = INT_MIN;
-        int last_sum = 0;
-        for (size_t i = 0; i < nums.size(); i++)
-        {
-            last_sum += nums[i];
-            if (max_sum < last_sum)
-            {
-                max_sum = last_sum;
-            }
-            if (last_sum < 0)
-            {
-                last_sum = 0;
-            }
-        }
-
-        return max_sum;
+class Solution {
+ public:
+  int maxSubArray(const std::vector<int>& nums) {
+    int max_sum = INT_MIN;
+    int last_sum = 0;
+    for (const int val : nums) {
+      last_sum += val;
+      if (max_sum < last_sum) {
+        max_sum = last_sum;
+      }
+      if (last_sum < 0) {
+        last_sum = 0;
+      }
     }
+
+    return max_sum;
+  }
 };
 
-int main(int argc, char* argv[])
-{
-    std::vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-    if (argc > 3)
-    {
-        nums.clear();
-        for (int i = 1; i < argc; i++)
-        {
-            nums.push_back(atoi(argv[i]));
-        }
+int main(int argc, char* argv[]) {
+  std::vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+  if (argc > 3) {
+    nums.clear();
+    for (int i = 1; i < argc; i++) {
+      nums.push_back(atoi(argv[i]));
     }
+  }
 
-    int max = Solution().maxSubArray(nums);
-    printf("%d\n", max);
+  int max = Solution().maxSubArray(nums);
+  printf("%d\n", max);
 
-    return 0;
+  return 0;
 }
