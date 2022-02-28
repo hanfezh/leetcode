1a23d299b043cb64dd3d8f824cbe3f918871fc3a
hanfezh <xianfeng.zhu@gmail.com>
2022-02-28 14:18:02 +0800

    Solve problem 228 by more pythonic way.

--- algorithms/summary_ranges.py
+++ algorithms/summary_ranges.py
@@ -9,11 +9,10 @@ class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         ranges = []
         for n in nums:
-            if ranges and ranges[-1] and (ranges[-1][-1] + 1 == n):
-                ranges[-1].append(n)
-            else:
-                ranges.append([n])
-        return [str(r[0]) if len(r) == 1 else f'{r[0]}->{r[-1]}' for r in ranges]
+            if not ranges or ranges[-1][-1] + 1 != n:
+                ranges.append([])
+            ranges[-1][1:] = n,
+        return ['->'.join(map(str, r)) for r in ranges]
 
 class TestSolution(unittest.TestCase):
     def test_summaryRanges(self):
