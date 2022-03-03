5373d068a57d48213d4a0376909771634e5ecbd4
hanfezh <xianfeng.zhu@gmail.com>
2022-03-03 11:04:11 +0800

    More pythonic way to determine subsequence, problem 392.

--- algorithms/is_subsequence.py
+++ algorithms/is_subsequence.py
@@ -4,7 +4,7 @@
 
 import unittest
 
-class Solution:
+class Solution1:
     def isSubsequence(self, s: str, t: str) -> bool:
         if len(s) > len(t):
             return False
@@ -16,9 +16,15 @@ class Solution:
             i += 1
         return True
 
+# More pythonic way
+class Solution2:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        t = iter(t)
+        return all(c in t for c in s)
+
 class TestSolution(unittest.TestCase):
     def test_isSubsequence(self):
-        sol = Solution()
+        sol = Solution2()
         self.assertTrue(sol.isSubsequence('abc', 'ahbgdc'))
         self.assertFalse(sol.isSubsequence('axc', 'ahbgdc'))
 
