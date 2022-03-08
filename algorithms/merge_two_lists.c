5c54bd9b7dc218d85c1b4cb2d13f3b51c37597cb
hanfezh <xianfeng.zhu@gmail.com>
2022-03-08 17:10:07 +0800

    Little optimization to merge two sorted lists.

--- algorithms/merge_two_lists.c
+++ algorithms/merge_two_lists.c
@@ -11,7 +11,7 @@
  *       Compiler:  gcc
  *
  *         Author:  Zhu Xianfeng <xianfeng.zhu@gmail.com>
- *   Organization:  
+ *   Organization:
  *
  * =====================================================================================
  */
@@ -19,109 +19,93 @@
 #include <stdlib.h>
 
 struct ListNode {
-	int val;
-	struct ListNode *next;
+  int val;
+  struct ListNode* next;
 };
 
 // Iterative
-struct ListNode *
-mergeTwoLists1(struct ListNode *l1, struct ListNode *l2)
-{
-	struct ListNode head;
-	struct ListNode *ptr = &head;
+struct ListNode* mergeTwoLists1(struct ListNode* l1, struct ListNode* l2) {
+  struct ListNode dummy;
+  struct ListNode* ptr = &dummy;
 
-	while (l1 || l2) {
-		if (l1 && l2) {
-			if (l1->val < l2->val) {
-				ptr->next = l1;
-				l1 = l1->next;
-			} else {
-				ptr->next = l2;
-				l2 = l2->next;
-			}
-		} else if (l1) {
-			ptr->next = l1;
-			l1 = l1->next;
-		} else {
-			ptr->next = l2;
-			l2 = l2->next;
-		}
-		ptr = ptr->next;
-	}
-
-	return head.next;
+  while (l1 && l2) {
+    if (l1->val < l2->val) {
+      ptr->next = l1;
+      l1 = l1->next;
+    } else {
+      ptr->next = l2;
+      l2 = l2->next;
+    }
+    ptr = ptr->next;
+  }
+  ptr->next = l1 ? l1 : l2;
+  return dummy.next;
 }
 
 // Recursive
-struct ListNode *
-mergeTwoLists2(struct ListNode *l1, struct ListNode *l2)
-{
-    if (l1 == NULL) {
-        return l2;
-    } else if (l2 == NULL) {
-        return l1;
-    }
+struct ListNode* mergeTwoLists2(struct ListNode* l1, struct ListNode* l2) {
+  if (l1 == NULL) {
+    return l2;
+  } else if (l2 == NULL) {
+    return l1;
+  }
 
-    if (l1->val < l2->val) {
-        l1->next = mergeTwoLists2(l1->next, l2);
-        return l1;
+  if (l1->val < l2->val) {
+    l1->next = mergeTwoLists2(l1->next, l2);
+    return l1;
+  } else {
+    l2->next = mergeTwoLists2(l1, l2->next);
+    return l2;
+  }
+}
+
+void generateList(struct ListNode** head, int* arr, int n) {
+  struct ListNode* ptr = NULL;
+  struct ListNode* tmp;
+  int i;
+
+  for (i = 0; i < n; i++) {
+    tmp = calloc(1, sizeof(struct ListNode));
+    tmp->val = arr[i];
+    if (!ptr) {
+      ptr = tmp;
     } else {
-        l2->next = mergeTwoLists2(l1, l2->next);
-        return l2;
+      ptr->next = tmp;
+      ptr = tmp;
     }
+    if (!*head) {
+      *head = tmp;
+    }
+  }
 }
 
-void 
-generateList(struct ListNode **head, int *arr, int n)
-{
-	struct ListNode *ptr = NULL;
-	struct ListNode *tmp;
-	int i;
+void printList(struct ListNode* head) {
+  struct ListNode* ptr = head;
 
-	for (i = 0; i < n; i++) {
-		tmp = calloc(1, sizeof(struct ListNode));
-		tmp->val = arr[i];
-		if (!ptr) {
-			ptr = tmp;
-		} else {
-			ptr->next = tmp;
-			ptr = tmp;
-		}
-		if (!*head) {
-			*head = tmp;
-		}
-	}
-}
-
-void printList(struct ListNode *head)
-{
-	struct ListNode *ptr = head;
-
-	printf("List: ");
-	while (ptr) {
-		printf("%d ", ptr->val);
-		ptr = ptr->next;
-	}
-	printf("\n");
+  printf("List: ");
+  while (ptr) {
+    printf("%d ", ptr->val);
+    ptr = ptr->next;
+  }
+  printf("\n");
 }
 
 #define mergeTwoLists mergeTwoLists2
 
-int main(int argc, char *argv[])
-{
-	struct ListNode *l1 = NULL;
-	struct ListNode *l2 = NULL;
-	struct ListNode *l3 = NULL;
-	int arr1[] = {1, 3, 5};
-	int arr2[] = {2, 4, 6};
+int main(int argc, char* argv[]) {
+  struct ListNode* l1 = NULL;
+  struct ListNode* l2 = NULL;
+  struct ListNode* l3 = NULL;
+  int arr1[] = {1, 3, 5};
+  int arr2[] = {2, 4, 6};
 
-	generateList(&l1, arr1, 3);
-	generateList(&l2, arr2, 3);
-	printList(l1);
-	printList(l2);
+  generateList(&l1, arr1, 3);
+  generateList(&l2, arr2, 3);
+  printList(l1);
+  printList(l2);
 
-	l3 = mergeTwoLists(l1, l2);
-	printList(l3);
+  l3 = mergeTwoLists(l1, l2);
+  printList(l3);
 
-	return 0;
+  return 0;
 }
