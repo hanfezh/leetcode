4e22385e1ac6075c72b067fd1bdfe8a29d167910
hanfezh <xianfeng.zhu@gmail.com>
2023-02-01 22:41:25 +0800

    Search insert position by binary search.

--- algorithms/search_insert_position.cpp
+++ algorithms/search_insert_position.cpp
@@ -3,8 +3,8 @@
  *
  *       Filename:  search_insert_position.cpp
  *
- *    Description:  Search Insert Position: Given a sorted array and a target value, 
- *                  return the index if the target is found. If not, return the index 
+ *    Description:  Search Insert Position: Given a sorted array and a target value,
+ *                  return the index if the target is found. If not, return the index
  *                  where it would be if it were inserted in order.
  *
  *        Version:  1.0
@@ -13,45 +13,44 @@
  *       Compiler:  gcc
  *
  *         Author:  Zhu Xianfeng (), xianfeng.zhu@gmail.com
- *   Organization:  
+ *   Organization:
  *
  * =====================================================================================
  */
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
 #include <vector>
 
+// Binary search
 class Solution {
-public:
-    int searchInsert(const std::vector<int>& nums, int target)
-    {
-        size_t i;
-        for (i = 0; i < nums.size(); i++)
-        {
-            if (nums[i] >= target)
-            {
-                break;
-            }
-        }
-
-        return i;
+ public:
+  int searchInsert(const std::vector<int>& nums, int target) {
+    int i = 0;
+    int j = nums.size() - 1;
+    while (i <= j) {
+      const int k = (i + j) / 2;
+      if (nums[k] > target) {
+        j = k - 1;
+      } else if (nums[k] < target) {
+        i = k + 1;
+      } else {
+        return k;
+      }
     }
+    return i;
+  }
 };
 
-int main(int argc, char* argv[])
-{
-    std::vector<int> nums = {1, 2, 3};
-    if (argc > 3)
-    {
-        nums.clear();
-        for (int i = 1; i < argc; i++)
-        {
-            nums.push_back(atoi(argv[i]));
-        }
+int main(int argc, char* argv[]) {
+  std::vector<int> nums = {1, 2, 3};
+  if (argc > 3) {
+    nums.clear();
+    for (int i = 1; i < argc; i++) {
+      nums.push_back(atoi(argv[i]));
     }
+  }
 
-    int idx = Solution().searchInsert(nums, 2);
-    printf("%d\n", idx);
-
-    return 0;
+  int idx = Solution().searchInsert(nums, 2);
+  printf("%d\n", idx);
+  return 0;
 }
