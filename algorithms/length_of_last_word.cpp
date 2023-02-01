daec9bb8eae96d625b474f318f6e9e960528c8f8
hanfezh <xianfeng.zhu@gmail.com>
2023-02-01 22:12:20 +0800

    Micro optimization to find the length of last word.

--- algorithms/length_of_last_word.cpp
+++ algorithms/length_of_last_word.cpp
@@ -3,8 +3,8 @@
  *
  *       Filename:  length_of_last_word.cpp
  *
- *    Description:  Length of Last Word. Given a string s consists of upper/lower-case 
- *                  alphabets and empty space characters ' ', return the length of last 
+ *    Description:  Length of Last Word. Given a string s consists of upper/lower-case
+ *                  alphabets and empty space characters ' ', return the length of last
  *                  word in the string.
  *
  *        Version:  1.0
@@ -13,55 +13,39 @@
  *       Compiler:  gcc
  *
  *         Author:  Zhu Xianfeng (), xianfeng.zhu@gmail.com
- *   Organization:  
+ *   Organization:
  *
  * =====================================================================================
  */
-#include <stdio.h>
+#include <cstdio>
 #include <string>
 
-class Solution
-{
-public:
-    int lengthOfLastWord(const std::string& str)
-    {
-        int prev = -1;
-        int len = 0;
-        int i = 0;
-
-        for (i = 0; i < str.size(); i++)
-        {
-            if (prev == -1 && str[i] != ' ')
-            {
-                prev = i;
-            }
-            else if (prev != -1 && str[i] == ' ')
-            {
-                len = i - prev;
-                prev = -1;
-            }
-        }
-
-        if (prev != -1 && i == str.size())
-        {
-            len = i - prev;
-        }
-
-        return len;
+class Solution {
+ public:
+  int lengthOfLastWord(const std::string& str) {
+    int j = str.size() - 1;
+    while (j >= 0 && str[j] == ' ') {
+      j--;
     }
+
+    int i = j;
+    while (i >= 0 && str[i] != ' ') {
+      i--;
+    }
+
+    return (j - i);
+  }
 };
 
-int main(int argc, char* argv[])
-{
-    std::string str = "Hello World";
-    if (argc > 1)
-    {
-        str = argv[1];
-    }
+int main(int argc, char* argv[]) {
+  std::string str = "Hello World";
+  if (argc > 1) {
+    str = argv[1];
+  }
 
-    auto len = Solution().lengthOfLastWord(str);
-    printf("Input: `%s`\nLength of last word: %d\n", str.c_str(), len);
+  auto len = Solution().lengthOfLastWord(str);
+  printf("Input: `%s`\nLength of last word: %d\n", str.c_str(), len);
 
-    return 0;
+  return 0;
 }
 
