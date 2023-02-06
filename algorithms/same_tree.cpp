f73b80654acdad9f83058c761533972ba748c2a4
hanfezh <xianfeng.zhu@gmail.com>
2023-02-06 11:04:22 +0800

    Preorder traversal to check same tree, problem #100.

--- algorithms/same_tree.cpp
+++ algorithms/same_tree.cpp
@@ -3,7 +3,7 @@
  *
  *       Filename:  same_tree.cpp
  *
- *    Description:  Same Tree. Given two binary trees, write a function to check if they 
+ *    Description:  Same Tree. Given two binary trees, write a function to check if they
  *                  are the same or not.
  *
  *        Version:  1.0
@@ -12,49 +12,79 @@
  *       Compiler:  gcc
  *
  *         Author:  Zhu Xianfeng (), xianfeng.zhu@gmail.com
- *   Organization:  
+ *   Organization:
  *
  * =====================================================================================
  */
-#include <stdio.h>
+#include <cstdio>
+#include <queue>
 
-struct TreeNode
-{
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 };
 
-class Solution
-{
-public:
-    bool isSameTree(TreeNode* p, TreeNode* q)
-    {
-        if (p == NULL && q == NULL)
-        {
-            return true;
-        }
-        else if (p == NULL || q == NULL)
-        {
-            return false;
-        }
-
-        if (p->val != q->val)
-        {
-            return false;
-        }
-
-        return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+// Recursion
+class Solution1 {
+ public:
+  bool isSameTree(TreeNode* p, TreeNode* q) {
+    if (p == nullptr && q == nullptr) {
+      return true;
+    } else if (p == nullptr || q == nullptr) {
+      return false;
+    } else if (p->val != q->val) {
+      return false;
     }
+    return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+  }
 };
 
-int main(int argc, char* argv[])
-{
-    TreeNode* a = NULL;
-    TreeNode* b = NULL;
-    auto is_same = Solution().isSameTree(a, b);
-    printf("Same tree? %s\n", (is_same ? "Yes" : "No"));
+// Iteration, preorder traversal
+class Solution2 {
+ public:
+  bool isSameTree(TreeNode* p, TreeNode* q) {
+    std::queue<TreeNode*> p_nodes;
+    std::queue<TreeNode*> q_nodes;
+    p_nodes.push(p);
+    q_nodes.push(q);
+    while (!p_nodes.empty()) {
+      p = p_nodes.front();
+      q = q_nodes.front();
+      if (!checkNode(p, q)) {
+        return false;
+      }
+      p_nodes.pop();
+      q_nodes.pop();
+      if (p != nullptr) {
+        p_nodes.push(p->left);
+        p_nodes.push(p->right);
+        q_nodes.push(q->left);
+        q_nodes.push(q->right);
+      }
+    }
+    return true;
+  }
 
-    return 0;
+ private:
+  bool checkNode(TreeNode* p, TreeNode* q) {
+    if (p == nullptr && q == nullptr) {
+      return true;
+    } else if (p == nullptr || q == nullptr) {
+      return false;
+    } else {
+      return (p->val == q->val);
+    }
+  }
+};
+
+int main(int argc, char* argv[]) {
+  TreeNode* a = nullptr;
+  TreeNode* b = nullptr;
+  auto is_same = Solution2().isSameTree(a, b);
+  printf("Same tree? %s\n", (is_same ? "Yes" : "No"));
+  return 0;
 }
