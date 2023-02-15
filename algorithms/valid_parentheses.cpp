3985cf286ebda6c543ac949cf658881d5b00525e
hanfezh <xianfeng.zhu@gmail.com>
2023-02-15 21:51:11 +0800

    More unit test cases for valid parentheses, problem #20.

--- algorithms/valid_parentheses.cpp
+++ algorithms/valid_parentheses.cpp
@@ -1,39 +1,40 @@
 // 20. Valid Parentheses: https://leetcode.com/problems/valid-parentheses
 // Author: xianfeng.zhu@gmail.com
-
-#include <stdio.h>
 #include <stack>
 #include <string>
+#include <vector>
 
-class Solution
-{
-public:
-    bool isValid(const std::string& s)
-    {
-        std::stack<char> chrs;
-        for (const auto c: s)
-        {
-            if (c == '(') {
-                chrs.push(')');
-            } else if (c == '[') {
-                chrs.push(']');
-            } else if (c == '{') {
-                chrs.push('}');
-            } else if (!chrs.empty() && chrs.top() == c) {
-                chrs.pop();
-            } else {
-                return false;
-            }
-        }
-        return chrs.empty();
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+class Solution {
+ public:
+  bool isValid(const std::string& s) {
+    std::stack<char> chrs;
+    for (const auto c : s) {
+      if (c == '(') {
+        chrs.push(')');
+      } else if (c == '[') {
+        chrs.push(']');
+      } else if (c == '{') {
+        chrs.push('}');
+      } else if (!chrs.empty() && chrs.top() == c) {
+        chrs.pop();
+      } else {
+        return false;
+      }
     }
+    return chrs.empty();
+  }
 };
 
-int main(int argc, char* argv[])
-{
-    std::string s = "{[()]}";
-    bool is_valid = Solution().isValid(s);
-    printf("Input: %s\n", s.c_str());
-    printf("Output: %s\n", (is_valid ? "true" : "false"));
-    return 0;
+TEST(Solution, isValud) {
+  std::vector<std::pair<std::string, bool>> cases = {
+      std::make_pair(std::string("()"), true),
+      std::make_pair(std::string("()[]{}"), true),
+      std::make_pair(std::string("(]"), false),
+  };
+  for (auto& c : cases) {
+    EXPECT_EQ(Solution().isValid(c.first), c.second);
+  }
 }
