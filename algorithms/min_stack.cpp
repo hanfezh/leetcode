31b3b98fda39e79d2c2cc9e312741665fbbc0872
hanfezh <xianfeng.zhu@gmail.com>
2023-02-19 08:30:21 +0800

    Micro optimization to design min stack, problem #155.

--- algorithms/min_stack.cpp
+++ algorithms/min_stack.cpp
@@ -1,67 +1,53 @@
 // 155. Min Stack: https://leetcode.com/problems/min-stack
 // Author: xianfeng.zhu@gmail.com
 
-#include <stdio.h>
-#include <limits.h>
-#include <assert.h>
 #include <algorithm>
+#include <climits>
 
-class MinStack
-{
-    struct Node
-    {
-        int val;
-        int min;
-        Node* next;
-        Node(int _val, int _min, Node* _next = nullptr):
-            val(_val), min(_min), next(_next) { }
-    };
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
 
-public:
-    /** initialize your data structure here. */
-    MinStack(): head_(nullptr), min_(INT_MAX)
-    {
-    }
-    
-    void push(int x)
-    {
-        min_ = std::min(x, min_);
-        head_ = new Node(x, min_, head_);
-    }
-    
-    void pop()
-    {
-        auto* ptr = head_;
-        head_ = head_->next;
-        min_ = ((head_ != nullptr) ? head_->min : INT_MAX);
-        delete ptr;
-    }
-    
-    int top()
-    {
-        return head_->val;
-    }
-    
-    int getMin()
-    {
-        return head_->min;
-    }
+class MinStack {
+  struct Node {
+    int val;
+    int min;
+    Node* next;
+    Node(int _val, int _min, Node* _next = nullptr) : val(_val), min(_min), next(_next) {}
+  };
 
-private:
-    Node* head_;
-    int min_;
+ public:
+  MinStack() : head_(nullptr) {}
+
+  void push(int x) {
+    if (head_ == nullptr) {
+      head_ = new Node(x, x);
+    } else {
+      head_ = new Node(x, std::min(head_->min, x), head_);
+    }
+  }
+
+  void pop() {
+    auto* ptr = head_;
+    head_ = head_->next;
+    delete ptr;
+  }
+
+  int top() { return head_->val; }
+
+  int getMin() { return head_->min; }
+
+ private:
+  Node* head_;
 };
 
-int main(int argc, char* argv[])
-{
+TEST(Solution, MinStack) {
+  MinStack min_stack;
+  min_stack.push(-2);
+  min_stack.push(0);
+  min_stack.push(-3);
+  EXPECT_EQ(min_stack.getMin(), -3);
 
-    MinStack min_stack;
-    min_stack.push(-2);
-    min_stack.push(0);
-    min_stack.push(-3);
-    assert(min_stack.getMin() == -3);
-    min_stack.pop();
-    assert(min_stack.top() == 0);
-    assert(min_stack.getMin() == -2);
-    return 0;
+  min_stack.pop();
+  EXPECT_EQ(min_stack.top(), 0);
+  EXPECT_EQ(min_stack.getMin(), -2);
 }
