1f281f88b13a4f072bef944c2200dcf417d040d0
hanfezh <xianfeng.zhu@gmail.com>
2023-03-08 09:13:59 +0800

    Micro optimization and more unit tests to find duplicate number.

--- algorithms/find_the_duplicate_number.cpp
+++ algorithms/find_the_duplicate_number.cpp
@@ -3,10 +3,8 @@
  *
  *       Filename:  find_the_duplicate_number.cpp
  *
- *    Description:  Find the Duplicate Number. Given an array nums containing n + 1 
- *                  integers where each integer is between 1 and n (inclusive), prove 
- *                  that at least one duplicate number must exist. Assume that there is 
- *                  only one duplicate number, find the duplicate one.
+ *    Description:  287. Find the Duplicate Number.
+ *                  https://leetcode.com/problems/find-the-duplicate-number/
  *
  *        Version:  1.0
  *        Created:  03/05/19 11:34:18
@@ -14,92 +12,71 @@
  *       Compiler:  gcc
  *
  *         Author:  Zhu Xianfeng (), xianfeng.zhu@gmail.com
- *   Organization:  
+ *   Organization:
  *
  * =====================================================================================
  */
-#include <stdio.h>
-#include <stdlib.h>
+#include <algorithm>
+#include <utility>
 #include <vector>
 
-// Modify array
-class Solution1
-{
-public:
-    int findDuplicate(std::vector<int>& nums)
-    {
-        int i = 0;
-        while (i < nums.size())
-        {
-            int j = nums[i];
-            if (j <= 0 || j >= nums.size())
-            {
-                break;
-            }
-            if (j == nums[j])
-            {
-                // Found duplicate
-                return j;
-            }
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
 
-            swap(&nums[i], &nums[j]);
-        }
-
-        return 0;
+// Modify array, time O(N), space O(1)
+class Solution1 {
+ public:
+  int findDuplicate(std::vector<int>& nums) {
+    const int i = 0;
+    while (i < nums.size()) {
+      const int j = nums[i];
+      if (j <= 0 || j >= nums.size()) {
+        break;
+      }
+      if (j == nums[j]) {
+        // Found duplicate
+        return j;
+      }
+      std::swap(nums[i], nums[j]);
     }
-
-private:
-    void swap(int* a, int* b)
-    {
-        *a ^= *b;
-        *b ^= *a;
-        *a ^= *b;
-    }
-};
-
-// Two pointers
-class Solution2
-{
-public:
-    int findDuplicate(std::vector<int>& nums)
-    {
-        int slow = nums[0];
-        int fast = nums[nums[0]];
-
-        // Start first meet
-        while (slow != fast)
-        {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        }
-
-        // Start second meet
-        fast = 0;
-        while (slow != fast)
-        {
-            slow = nums[slow];
-            fast = nums[fast];
-        }
-
-        // The second meet point is the duplicate
-        return slow;
-    }
-};
-
-using Solution = Solution2;
-
-int main(int argc, char* argv[])
-{
-    std::vector<int> nums = {1, 3, 4, 2, 2};
-    if (argc > 1)
-    {
-        nums.clear();
-        for (int i = 1; i < argc; i++)
-        {
-            nums.push_back(atoi(argv[i]));
-        }
-    }
-    auto dup_num = Solution().findDuplicate(nums);
-    printf("Duplicate number: %d\n", dup_num);
     return 0;
+  }
+};
+
+// Two pointers, time O(N), space O(1)
+class Solution2 {
+ public:
+  int findDuplicate(std::vector<int>& nums) {
+    int slow = nums[0];
+    int fast = nums[nums[0]];
+
+    // Start first meet
+    while (slow != fast) {
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+    }
+
+    // Start second meet
+    fast = 0;
+    while (slow != fast) {
+      slow = nums[slow];
+      fast = nums[fast];
+    }
+
+    // The second meet point is the duplicate
+    return slow;
+  }
+};
+
+TEST(Solution, findDuplicate) {
+  std::vector<std::pair<std::vector<int>, int>> cases = {
+      std::make_pair(std::vector<int>{1, 3, 4, 2, 2}, 2),
+      std::make_pair(std::vector<int>{3, 1, 3, 4, 2}, 3),
+  };
+  for (auto& c : cases) {
+    auto c1 = c;
+    auto c2 = c;
+    EXPECT_EQ(Solution1().findDuplicate(c1.first), c1.second);
+    EXPECT_EQ(Solution2().findDuplicate(c2.first), c2.second);
+  }
 }
