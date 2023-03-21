ba1c7f0cc1692ba991cc502a1d51d08974330d12
hanfezh <xianfeng.zhu@gmail.com>
2023-03-21 10:35:03 +0800

    More unit test cases for valid palindrome II, problem #680.

--- algorithms/valid_palindrome2.cpp
+++ algorithms/valid_palindrome2.cpp
@@ -3,8 +3,8 @@
  *
  *       Filename:  valid_palindrome2.cpp
  *
- *    Description:  680. Valid Palindrome II. Given a non-empty string s, you may delete 
- *                  at most one character. Judge whether you can make it a palindrome.
+ *    Description:  680. Valid Palindrome II. Given a non-empty string s, you
+ * may delete at most one character. Judge whether you can make it a palindrome.
  *
  *        Version:  1.0
  *        Created:  04/08/19 11:48:42
@@ -12,60 +12,54 @@
  *       Compiler:  gcc
  *
  *         Author:  Zhu Xianfeng (), xianfeng.zhu@gmail.com
- *   Organization:  
+ *   Organization:
  *
  * =====================================================================================
  */
-#include <stdio.h>
-#include <stdlib.h>
 #include <string>
+#include <utility>
+#include <vector>
 
-class Solution
-{
-public:
-    bool validPalindrome(const std::string& s)
-    {
-        int i = 0;
-        int j = s.size() - 1;
-        while (i < j)
-        {
-            if (s.at(i) != s.at(j))
-            {
-                return isPalindrome(s, i + 1, j) || 
-                       isPalindrome(s, i, j - 1);
-            }
-            i++;
-            j--;
-        }
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
 
-        return true;
+using std::string;
+using std::vector;
+
+// Two pointers
+class Solution {
+ public:
+  bool validPalindrome(const string& s) {
+    int i = 0;
+    int j = s.size() - 1;
+    while (i < j) {
+      if (s.at(i) != s.at(j)) {
+        return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+      }
+      i++;
+      j--;
     }
+    return true;
+  }
 
-private:
-    bool isPalindrome(const std::string &s, int i, int j)
-    {
-        while (i < j)
-        {
-            if (s.at(i) != s.at(j))
-            {
-                return false;
-            }
-            i++;
-            j--;
-        }
-
-        return true;
+ private:
+  bool isPalindrome(const string& s, int i, int j) {
+    while (i < j) {
+      if (s.at(i++) != s.at(j--)) {
+        return false;
+      }
     }
+    return true;
+  }
 };
 
-int main(int argc, char* argv[])
-{
-    std::string s = "abca";
-    if (argc > 1)
-    {
-        s = argv[1];
-    }
-    auto valid = Solution().validPalindrome(s);
-    printf("Input: `%s`.\nIs valid palindrome? %s\n", s.c_str(), (valid ? "Yes" : "No"));
-    return 0;
+TEST(Solution, validPalindrome) {
+  vector<std::pair<string, bool>> cases = {
+      std::make_pair(string("abc"), false),
+      std::make_pair(string("abca"), true),
+      std::make_pair(string("aabaca"), true),
+  };
+  for (auto& c : cases) {
+    EXPECT_EQ(Solution().validPalindrome(c.first), c.second);
+  }
 }
