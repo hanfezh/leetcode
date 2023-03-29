14628b41a860bf103aed0c5d451639d662e82926
hanfezh <xianfeng.zhu@gmail.com>
2023-03-29 11:17:24 +0800

    Optimized solution for three sum without set, problem #15.

--- algorithms/three_sum.cpp
+++ algorithms/three_sum.cpp
@@ -27,7 +27,7 @@ using std::set;
 using std::vector;
 
 // Sorting + Two Pointers
-class Solution {
+class Solution1 {
  public:
   vector<vector<int>> threeSum(vector<int>& nums) {
     std::sort(nums.begin(), nums.end());
@@ -51,6 +51,41 @@ class Solution {
   }
 };
 
+// Sorting + Two Pointers, without std::set
+class Solution2 {
+ public:
+  vector<vector<int>> threeSum(vector<int>& nums) {
+    std::sort(nums.begin(), nums.end());
+    vector<vector<int>> triplets;
+    const int size = nums.size();
+    for (int i = 0; i < size - 2; i++) {
+      int l = i + 1;
+      int r = size - 1;
+      while (l < r) {
+        const int sum = nums[i] + nums[l] + nums[r];
+        if (sum == 0) {
+          vector<int> t{nums[i], nums[l++], nums[r--]};
+          while (l < r && nums[l] == t[1]) {
+            l++;
+          }
+          while (l < r && nums[r] == t[2]) {
+            r--;
+          }
+          triplets.push_back(std::move(t));
+        } else if (sum < 0) {
+          l++;
+        } else {
+          r--;
+        }
+      }
+      while ((i + 1) < size && nums[i] == nums[i + 1]) {
+        i++;  // Ignore duplicated number
+      }
+    }
+    return vector<vector<int>>(triplets.begin(), triplets.end());
+  }
+};
+
 TEST(Solution, threeSum) {
   vector<std::pair<vector<int>, vector<vector<int>>>> cases = {
       std::make_pair(vector<int>{0, 1, 1}, vector<vector<int>>{}),
@@ -59,6 +94,9 @@ TEST(Solution, threeSum) {
                      vector<vector<int>>{{-1, -1, 2}, {-1, 0, 1}}),
   };
   for (auto& c : cases) {
-    EXPECT_THAT(Solution().threeSum(c.first), testing::ElementsAreArray(c.second));
+    auto c1 = c;
+    auto c2 = c;
+    EXPECT_THAT(Solution1().threeSum(c1.first), testing::ElementsAreArray(c1.second));
+    EXPECT_THAT(Solution2().threeSum(c2.first), testing::ElementsAreArray(c2.second));
   }
 }
