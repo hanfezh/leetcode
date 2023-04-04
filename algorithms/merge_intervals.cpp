1c127d6b491ba5a9b3735f7a08b805250315221d
hanfezh <xianfeng.zhu@gmail.com>
2023-04-04 21:51:48 +0800

    More unit tests for merge intervals, problem #56.

--- algorithms/merge_intervals.cpp
+++ algorithms/merge_intervals.cpp
@@ -3,7 +3,7 @@
  *
  *       Filename:  merge_intervals.cpp
  *
- *    Description:  56. Merge Intervals. 
+ *    Description:  56. Merge Intervals.
  *                  Given a collection of intervals, merge all overlapping intervals.
  *
  *        Version:  1.0
@@ -12,73 +12,62 @@
  *       Compiler:  gcc
  *
  *         Author:  Zhu Xianfeng (), xianfeng.zhu@gmail.com
- *   Organization:  
+ *   Organization:
  *
  * =====================================================================================
  */
-#include <stdio.h>
-#include <stdlib.h>
 #include <algorithm>
+#include <utility>
 #include <vector>
 
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+
 // Definition for an interval.
-struct Interval
-{
-    int start;
-    int end;
-    Interval() : start(0), end(0) {}
-    Interval(int s, int e) : start(s), end(e) {}
+struct Interval {
+  int start;
+  int end;
+  Interval() : start(0), end(0) {}
+  Interval(int s, int e) : start(s), end(e) {}
 };
 
-class Solution
-{
-public:
-    std::vector<Interval> merge(std::vector<Interval>& intervals)
-    {
-        auto cmp_func = [](const Interval& a, const Interval& b) -> bool {
-            return a.start < b.start;
-        };
-        std::sort(intervals.begin(), intervals.end(), cmp_func);
-        
-        std::vector<Interval> merges;
-        for (int i = 0; i < intervals.size(); )
-        {
-            int low = intervals[i].start;
-            int high = intervals[i].end;
-            int next = i + 1;
-            while ((next < intervals.size()) && (intervals[next].start <= high))
-            {
-                high = std::max(high, intervals[next].end);
-                next++;
-            }
+bool operator==(const Interval& a, const Interval& b) {
+  return (a.start == b.start && a.end == b.end);
+}
 
-            i = next;
-            merges.push_back(Interval(low, high));
-        }
+class Solution {
+ public:
+  std::vector<Interval> merge(std::vector<Interval>& intervals) {
+    auto cmp_func = [](const Interval& a, const Interval& b) -> bool { return a.start < b.start; };
+    std::sort(intervals.begin(), intervals.end(), cmp_func);
 
-        return merges;
+    std::vector<Interval> merges;
+    for (int i = 0; i < intervals.size();) {
+      int low = intervals[i].start;
+      int high = intervals[i].end;
+      int next = i + 1;
+      while ((next < intervals.size()) && (intervals[next].start <= high)) {
+        high = std::max(high, intervals[next].end);
+        next++;
+      }
+
+      i = next;
+      merges.push_back(Interval(low, high));
     }
+
+    return merges;
+  }
 };
 
-int main(int argc, char* argv[])
-{
-    std::vector<Interval> intervals = {
-        Interval(1, 3),
-        Interval(2, 6),
-        Interval(8, 10),
-        Interval(15, 18),
-    };
-
-    auto merges = Solution().merge(intervals);
-    for (int i = 0; i < merges.size(); i++)
-    {
-        if (i != 0)
-        {
-            printf(" ,");
-        }
-        printf("[%d, %d]", merges[i].start, merges[i].end);
-    }
-    printf("\n");
-
-    return 0;
+TEST(Solution, merge) {
+  vector<pair<vector<Interval>, vector<Interval>>> cases = {
+      make_pair(vector<Interval>{{1, 3}, {2, 6}, {8, 10}, {15, 18}},
+                vector<Interval>{{1, 6}, {8, 10}, {15, 18}}),
+      make_pair(vector<Interval>{{1, 4}, {4, 5}}, vector<Interval>{{1, 5}}),
+  };
+  for (auto& c : cases) {
+    EXPECT_THAT(Solution().merge(c.first), testing::UnorderedElementsAreArray(c.second));
+  }
 }
