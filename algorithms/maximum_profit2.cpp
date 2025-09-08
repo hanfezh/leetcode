7431197dc23961ff746c305dd5dbd97079cdb2c7
hanfezh <xianfeng.zhu@gmail.com>
2025-09-08 10:38:16 +0800

    Best time to buy and sell stock II #122

--- algorithms/maximum_profit2.cpp
+++ algorithms/maximum_profit2.cpp
@@ -11,91 +11,59 @@
  *       Compiler:  gcc
  *
  *         Author:  Zhu Xianfeng (), xianfeng.zhu@gmail.com
- *   Organization:  
+ *   Organization:
  *
  * =====================================================================================
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
+
 #include <vector>
 
+#include "gtest/gtest.h"
+
 // Peak Valley Approach
-class Solution1
-{
-public:
-    int maxProfit(const std::vector<int>& prices)
-    {
-        if (prices.size() == 0)
-        {
-            return 0;
-        }
+class Solution1 {
+ public:
+  int maxProfit(std::vector<int>& prices) {
+    int max_profit = 0;
+    int min_price = prices[0];
+    int max_price = prices[0];
 
-        int min_price = prices[0];
-        int max_price = 0;
-        int max_profit = 0;
-
-        for (size_t i = 1; i < prices.size(); i++)
-        {
-            if (prices[i] >= prices[i - 1])
-            {
-                max_price = prices[i];
-            }
-            else
-            {
-                if (max_price > min_price)
-                {
-                    max_profit += max_price - min_price;
-                }
-                min_price = prices[i];
-                max_price = 0;
-            }
-        }
-
-        if (max_price > min_price)
-        {
-            max_profit += max_price - min_price;
-        }
-
-        return max_profit;
+    for (int i = 1; i < prices.size(); i++) {
+      if (prices[i] > max_price) {
+        max_price = prices[i];
+      } else {
+        max_profit += max_price - min_price;
+        min_price = prices[i];
+        max_price = prices[i];
+      }
     }
+    max_profit += max_price - min_price;
+    return max_profit;
+  }
 };
 
 // Optimized solution
-class Solution2
-{
-public:
-    int maxProfit(const std::vector<int>& prices)
-    {
-        int max_profit = 0;
-        for (int i = 1; i < prices.size(); i++)
-        {
-            if (prices[i] > prices[i - 1])
-            {
-                max_profit += prices[i] - prices[i - 1];
-            }
-        }
-        return max_profit;
+class Solution2 {
+ public:
+  int maxProfit(const std::vector<int>& prices) {
+    int max_profit = 0;
+    for (int i = 1; i < prices.size(); i++) {
+      if (prices[i] > prices[i - 1]) {
+        max_profit += prices[i] - prices[i - 1];
+      }
     }
+    return max_profit;
+  }
 };
 
-using Solution = Solution2;
-
-int main(int argc, char* argv[])
-{
-    // std::vector<int> nums = {7, 1, 5, 3, 6, 4};
-    std::vector<int> nums = {1, 2, 3, 4, 5};
-    if (argc > 3)
-    {
-        nums.clear();
-        for (int i = 1; i < argc; i++)
-        {
-            nums.push_back(atoi(argv[i]));
-        }
-    }
-
-    int max = Solution().maxProfit(nums);
-    printf("%d\n", max);
-
-    return 0;
+TEST(Solution, maxProfit) {
+  std::vector<std::pair<std::vector<int>, int>> cases = {
+      {{7, 1, 5, 3, 6, 4}, 7},
+      {{1, 2, 3, 4, 5}, 4},
+      {{7, 6, 4, 3, 1}, 0},
+  };
+  for (auto& [prices, max_profit] : cases) {
+    EXPECT_EQ(Solution1().maxProfit(prices), max_profit);
+    EXPECT_EQ(Solution2().maxProfit(prices), max_profit);
+  }
 }
